4060fb2f5fee15ff06626b0abbdfe5267c263018
Manish282004 <132204894+Manish282004@users.noreply.github.com>
2023-04-30 18:02:35 +0530

    Add files via upload

---
+++ 7.C
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+int weekNum;
+printf("Enter the  week number(1-7):");
+scanf("%d",&weekNum);
+switch (weekNum)
+{
+case 1:
+printf("Monday\n");
+break;
+case 2:
+printf("Tuesday\n");
+break;
+case 3:
+printf("Wednesday\n");
+break;
+case 4:
+printf("Thursday\n");
+break;
+case 5:
+printf("Friday\n");
+break;
+case 6:
+printf("Saturday\n");
+break;
+case 7:
+printf("Sunday\n");
+break;
+default:
+printf("Invalid week number!\n");
+break;
+}
+return 0;
+}
+
+
